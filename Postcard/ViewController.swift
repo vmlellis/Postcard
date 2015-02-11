54f1fb739f9509d001d1b4214836268124427b11
Victor Mendonça Lellis <vmlellis@Victors-Mac.local>
2015-02-11 09:50:08 -0200

    second commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -13,23 +13,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func viewDidLayoutSubviews()
+    {
+        super.viewDidLayoutSubviews()
+        //messageLabel.numberOfLines = 0
+        //messageLabel.sizeToFit()
+    }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
         enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
     }
+    
+    
 
 }
 
